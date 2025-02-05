ddb2c4cd2b0a317875222b469e5ca423a25bfc34
kanzulrehman <kanzlala852@gmail.com>
2025-02-05 05:50:21 -0800

    Update day7.dart

--- day7.dart
+++ day7.dart
@@ -1,1 +1,60 @@
+           VARIABLE
+TYPE SYNTAX
+  var name='smith';
+Consider the following example_
+  void main() {
+     string name=1;
+} 
+EXAMPLE illustrates
+  void main() {
+     int num;
+    print(num);   
+} 
+Output
+  Null 
+THE DYNAMIC KEYWORD
+ example
+  void main() {
+     dynamic x= 'tom';
+     print(x);
+}
+OUTPUT
+  Tom
+  FINAL AND CONST
+ example:final keyword
 
+void main () {
+  final vall = 12;
+  print(vall);
+}
+OUTPUT:
+12
+  Example:const keyword 
+ void main () {
+  const pi   = 3.14;
+  const area = pi*12*12; 
+  print("the output is $ area");
+} 
+  OUTPUT
+The output is 452.15999999999997
+  CONST variables
+    example= 
+  void main() {
+   final   v1=12;
+   const   v2=13;
+    v2    =12;
+  }
+
+
+
+
+
+
+  
+
+
+
+
+
+
+  
