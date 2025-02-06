3bc8abaa69ceda0452661d87f0c941d5fab500a1
kanzulrehman <kanzlala852@gmail.com>
2025-02-06 07:56:57 -0800

    Create day8.dart

---
+++ day8.dart
@@ -0,0 +1,27 @@
+       Conditional Expression 
+ Example
+
+ void main() {
+  var a=10;
+  var res=a>12?"value greater than 10";"value lesser than or equal to 10";
+  print(res);
+Output
+  value lesser than or equal to 10
+           
+            Another example
+void main() {
+ var a=null;
+ var b=a??b;
+ print(res);
+}
+Output
+    12
+  
+
+
+
+
+
+
+
+
