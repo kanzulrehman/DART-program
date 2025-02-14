84916e60958777537986723cf76ca4defabd7f82
kanzulrehman <kanzlala852@gmail.com>
2025-02-14 06:14:13 -0800

    Create day18.dart

---
+++ day18.dart
@@ -0,0 +1,96 @@
+Example 
+      import'dart:core';
+      void main() {
+        f1();
+      }
+      f1() {
+      string x='rules';
+      print(x.code unit At(0));
+     }
+    Output
+       82
+string.code with property
+Example
+       import'dart:core';
+       void main() {
+        f1();
+      }
+        f1() {
+          string x='runes';
+          print(x.code unit);
+        } 
+Output
+ [82,117,110,101,115]
+String.runes property
+ example
+         void main () {
+            "A string".runes.for each((int rune) {
+               var character=new string.from char code(rune);
+               print(character)
+            });
+          }
+    Output
+     A
+     s  
+     t
+     r 
+     i 
+     n   
+     g
+EXAMPLE
+    main() {
+      rones input=newrones('\u{1f605}');
+      print(new string.form charcodes(input));
+    }
+
+
+
+
+
+
+
+
+
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
