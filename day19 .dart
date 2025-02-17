e136bf38f8936616907b9e176065cfb0dc23b964
kanzulrehman <kanzlala852@gmail.com>
2025-02-17 04:32:03 -0800

    Create day19 .dart

---
+++ day19 .dart
@@ -0,0 +1,39 @@
+Example
+
+     void main() {
+       print(factorial(6));
+      }
+      fractorial(number) {
+        if(number<=0) {
+        // termination case 
+          return 1;
+      } else {
+        return(number*factorial(number-1));
+        // function invokes itself
+      }
+   }
+  Output
+   720
+Example
+      void main() {
+      printMsg(); 
+      print(test());
+    }
+    printMsg()=>
+    print("hello");
+    int test()=123;
+    //returingfunction
+Output
+   hello 123
+
+
+
+
+
+
+
+
+
+
+
+
