d73b0c1a485cdb68cb366b9e1205e2040618ca08
kanzulrehman <kanzlala852@gmail.com>
2025-02-22 09:58:17 -0800

    Create day22.dart

---
+++ day22.dart
@@ -0,0 +1,130 @@
+Example 
+
+      class student {
+      void test_method() {
+        print("this is a test method")
+      }
+
+        void test_method1() { 
+          print("This is a  test method1"); 
+        }
+     }
+     void main() {
+       student S1=new student();
+       s1.test_method();
+       s1.test_method1();
+     }
+Output 
+     this is a test method
+     this is a test method1
+The cascade operater
+
+          class student {
+            void test_method() {
+              print("this is a test method");
+          }
+          void test_method1() {
+            print("this is a test method1");
+          }
+         }
+          void main() {
+          new student()
+          ..test_method()
+          ..test_method1();
+         }
+  output 
+   This is a test method 
+   This is a test method1
+
+the to string()method 
+
+     void main() {
+       int n=12;
+       print(n.to string());
+Output
+    12
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
