3aeaeb0b6a0d5694319891871f19ec7804394d15
kanzulrehman <kanzlala852@gmail.com>
2025-02-24 11:00:05 -0800

    Create day23.dart

---
+++ day23.dart
@@ -0,0 +1,62 @@
+                               PROGRAMMING COLLECTION
+Example 
+
+      import'dart:collection';
+      void main() {
+        queue numQ=new Queue();
+        numQ.addAll([100,200,300]);
+        itreator i=numQ.iterator;
+
+        white(i.move next()) {
+          print(i.current);
+        }
+       }
+ Output
+ 100
+ 200
+ 300
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
