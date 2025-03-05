2ce9cf550ecceb7812995e75d29aa19b8c62d912
kanzulrehman <kanzlala852@gmail.com>
2025-03-05 10:39:37 -0800

    Create day27.dart

---
+++ day27.dart
@@ -0,0 +1,58 @@
+Adding a break point:
+
+        void main() {
+          int a=10;b=20;c=5;
+          c=c*c*c;
+
+        print("$a+$b=${a+b}");
+        print("$a%$b=${a%b}");  //add a break point here
+        print("$a*$b=$b=${a+b}");
+        print("$a/$b=${a/b}");
+        print(c);
+        }
+ Output 
+ 10+20=30
+ 10%20=10
+ 10*20=200
+ 10/20=0.5
+ 125
+
+
+
+
+
+ 
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
